780305bfbe4a329f887a629fb56d220e21f2e782
zzhrex <zzhclare@foxmail.com>
2017-02-11 22:16:58 +0800

    add sidebar but remain bug

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+<meta charset="utf-8"/>
 <title>@yield('title')</title>
-<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('/css/semantic.min.css') }}" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Catamaran|Dancing+Script|Pinyon+Script|Righteous|Tangerine" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/semantic.css') }}">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Catamaran|Dancing+Script|Pinyon+Script|Righteous|Tangerine">
 @yield('additional_css')
 <!--
 <link href="https://fonts.googleapis.com/css?family=Catamaran|Dancing+Script|Merriweather|Montserrat|Pinyon+Script|Righteous|Tangerine" rel="stylesheet">
 -->
 <script src="{{ asset('/js/jquery.min.js') }}"></script>
-<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('/js/semantic.min.js') }}"></script>
 @yield('additional_js')
 </head>
