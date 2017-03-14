cb9b3496d9c397544aed7141685493c8797e3618
zzhrex <zzhclare@foxmail.com>
2017-03-14 22:09:41 +0800

    change the master container to grid

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -3,138 +3,51 @@
 @section('title', '博客管理')
 
 @section('content')
-<div class="ui segment" style="min-height:900px;">
-    <div class="ui divided items">
-        <div class="item">
-            <div class="ui small rounded image">
-                <img src="{{asset('images/gundam.jpg')}}">
-            </div>
-            <div class="content">
-                <a class="header">12 Years a Slave</a>
-                <div class="meta">
-                    <span class="cinema">Union Square 14</span>
+<div class="ui four wide column">
+    <div class="ui secondary vertical pointing menu" style="float:right">
+        <a class="active item" href="{{url('/management')}}")}}>Blog Management</a>
+        <a class="item">Label Management</a>
+        <a class="item">Blog Group Management</a>
+        <a class="item" href="{{url('/publish')}}">Publish A Blog</a>
+    </div>
+</div>
+<div class="ui twelve wide column">
+    <div class="ui segment container" style="min-height:900px;">
+        <div class="ui divided items">
+            <div class="item">
+                <div class="ui small rounded image">
+                    <img src="{{asset('images/gundam.jpg')}}">
                 </div>
-                <div class="description">
-                    <p></p>
-                </div>
-                <div class="extra">
-                    <div class="ui blue label">IMAX</div>
-                    <div class="ui yellow label"><i class="globe icon"></i> Additional Languages</div>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="ui small rounded image">
-                <img src="{{asset('images/gundam.jpg')}}">
-            </div>
-            <div class="content">
-                <a class="header">My Neighbor Totoro</a>
-                <div class="meta">
-                    <span class="cinema">IFC Cinema</span>
-                </div>
-                <div class="description">
-                    <p></p>
-                </div>
-                <div class="extra">
-                    <div class="ui right floated primary button">
-                        Detail
-                        <i class="right chevron icon"></i>
+                <div class="content">
+                    <a class="header">12 Years a Slave</a>
+                    <div class="meta">
+                        <span class="cinema">Union Square 14</span>
                     </div>
-                    <div class="ui red label">Limited</div>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="ui small rounded image">
-                <img src="{{asset('images/man.jpg')}}">
-            </div>
-            <div class="content">
-                <a class="header">Watchmen</a>
-                <div class="meta">
-                    <span class="cinema">IFC</span>
-                </div>
-                <div class="description">
-                    <p></p>
-                </div>
-                <div class="extra">
-                    <div class="ui right floated primary button">
-                        Detail
-                        <i class="right chevron icon"></i>
+                    <div class="description">
+                        <p></p>
+                    </div>
+                    <div class="extra">
+                        <div class="ui blue label">IMAX</div>
+                        <div class="ui yellow label"><i class="globe icon"></i> Additional Languages</div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="ui left rail">
-        <div class="ui sticky">
-            <div class="ui secondary vertical pointing menu">
-                <a class="active item" href="{{url('/management')}}")}}>Blog Management</a>
-                <a class="item">Label Management</a>
-                <a class="item">Blog Group Management</a>
-                <a class="item" href="{{url('/publish')}}">Publish A Blog</a>
-            </div>
-        </div>
-    </div>
-    <div class="ui right rail">
-        <div class="ui sticky">
-            <div class="ui card">
-                <div class="image">
+            <div class="item">
+                <div class="ui small rounded image">
                     <img src="{{asset('images/man.jpg')}}">
                 </div>
                 <div class="content">
-                    <a class="header">zzhRex</a>
+                    <a class="header">Watchmen</a>
                     <div class="meta">
-                        <span class="date">design at 2017</span>
+                        <span class="cinema">IFC</span>
                     </div>
-                    </br>
-                    </br>
-                    </br>
-                    </br>
-                    </br>
                     <div class="description">
-                        <div class="ui styled accordion">
-                            <div class="title">
-                                <i class="table icon"></i>
-                                文章分类
-                            </div>
-                            <div class="content">
-                                <p class="transition hidden">
-                                分类1
-                                </p>
-                                <p class="transition hidden">
-                                分类2
-                                </p>
-                                <p class="transition hidden">
-                                分类3
-                                </p>
-                                <p class="transition hidden">
-                                分类4
-                                </p>
-                            </div>
-                            <div class="title">
-                                <i class="calendar icon"></i>
-                                文章存档
-                            </div>
-                            <div class="content">
-                                <p class="transition hidden">
-                                2017-02-24
-                                </p>
-                                <p class="transition hidden">
-                                2017-02-25
-                                </p>
-                            </div>
-                            <div class="title">
-                                <i class="unhide icon"></i>
-                                阅读排行
-                            </div>
-                            <div class="content">
-                                <p class="transition hidden">
-                                article 1
-                                </p>
-                                <p class="transition hidden">
-                                article 2
-                                </p>
-                            </div>
+                        <p></p>
+                    </div>
+                    <div class="extra">
+                        <div class="ui right floated primary button">
+                            Detail
+                            <i class="right chevron icon"></i>
                         </div>
                     </div>
                 </div>
